bb59c1937dfe36e03c5a224ee4d7a80f8513872c
Awais Ahmad <38325321+AwaisAhmad1@users.noreply.github.com>
2021-01-28 13:43:15 +0500

    Project complete Red queen React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import {RedQueenRace} from './RedQueenRace';
+function App() {
+  return (
+    <div className="App">
+    <RedQueenRace />
+    </div>
+  );
+}
+
+export default App;
